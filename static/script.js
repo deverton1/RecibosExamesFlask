2e36f3b7226432530aa7e2880e12aa4b656f9af1
deverton1 <evertonrabelo113@gmail.com>
2024-08-06 16:15:29 -0300

    Adicionado copy e limpeza de código

--- static/script.js
+++ static/script.js
@@ -2,13 +2,10 @@ function calculateTotal() {
     const admissionais = document.getElementById('admissionais').value.split('\n').filter(name => name.trim() !== '');
     const demissionais = document.getElementById('demissionais').value.split('\n').filter(name => name.trim() !== '');
     const periodicos = document.getElementById('periodicos').value.split('\n').filter(name => name.trim() !== '');
-    
+
     const totalFuncionarios = admissionais.length + demissionais.length + periodicos.length;
     const precoExame = parseFloat(document.getElementById('precoExame').value);
     const valorTotal = totalFuncionarios * precoExame;
 
     document.getElementById('totalExames').value = `R$ ${valorTotal.toFixed(2)}`;
-}
-function consultaCnpj(){
-    URL=("https://solucoes.receita.fazenda.gov.br/Servicos/cnpjreva/Cnpjreva_Solicitacao.asp?cnpj=")
 }
